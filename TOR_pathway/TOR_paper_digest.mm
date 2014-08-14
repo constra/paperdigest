48ef59d7c9ed68d55d67af0a4690e32758a4684e
Sun Mai <sun.mai@outlook.com>
2014-08-14 16:07:00 +0200

    add mindmap for TOR_pathway

---
+++ TOR_pathway/TOR_paper_digest.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1408024907568" ID="ID_1553494727" MODIFIED="1408025018244" TEXT="TOR paper digest">
+<node CREATED="1408024957200" ID="ID_408854319" MODIFIED="1408025038410" POSITION="right" TEXT="Metabolism and disease">
+<node CREATED="1408025045509" ID="ID_1014937677" LINK="../../Off-line-reading/Cell%202012%20Laplante%20M.pdf" MODIFIED="1408025152821" TEXT="Laplante &amp; Sabatini 2012"/>
+</node>
+</node>
+</map>
