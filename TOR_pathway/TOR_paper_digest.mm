a642a2933755c5527792c47299be17d59d123ba5
Sun Mai <sun.mai@outlook.com>
2014-08-15 10:30:30 +0200

    mindmap changes

--- TOR_pathway/TOR_paper_digest.mm
+++ TOR_pathway/TOR_paper_digest.mm
@@ -11,33 +11,51 @@
 <node CREATED="1408026353198" ID="ID_1244339472" MODIFIED="1408026872256" TEXT="Sabatini, 1994, RAFT1: a mammalian protein that binds to FKBP12 in a rapamycin-dependent fashion and is homologous to yeast TORs"/>
 <node CREATED="1408026907698" ID="ID_1279481596" MODIFIED="1408026919325" TEXT="Sabers, 1995, Isolation of a protein target of the FKBP12-rapamycin complex in mammalian cells"/>
 </node>
-<node CREATED="1408027148272" ID="ID_132060930" MODIFIED="1408027151496" POSITION="right" TEXT="Structure">
+<node CREATED="1408027148272" ID="ID_132060930" MODIFIED="1408091097099" POSITION="right" TEXT="Structure">
 <node CREATED="1408027151497" ID="ID_1094006752" MODIFIED="1408027197040" TEXT="shared">
-<node CREATED="1408027154592" ID="ID_1155809635" MODIFIED="1408027161630" TEXT="mLST1">
+<node CREATED="1408027154592" ID="ID_1155809635" MODIFIED="1408091369355" TEXT="mLST1">
 <node CREATED="1408027161632" ID="ID_483991749" MODIFIED="1408027244269" TEXT="Jacinto, 2004, Mammalian TOR complex 2 controls the actin cytoskeleton and is rapamycin insensitive"/>
 <node CREATED="1408028600185" ID="ID_755988488" MODIFIED="1408028614049" TEXT="Kim, 2003, GbetaL, a positive regulator of the rapamycin-sensitive pathway required for the nutrient-sensitive interaction between raptor and mTOR."/>
 </node>
 <node CREATED="1408027211851" ID="ID_480555349" MODIFIED="1408027214758" TEXT="mTOR"/>
-<node CREATED="1408028695888" ID="ID_1785618384" MODIFIED="1408028699516" TEXT="DEPTOR">
+<node CREATED="1408028695888" ID="ID_1785618384" MODIFIED="1408091368629" TEXT="DEPTOR">
 <node CREATED="1408028699518" ID="ID_340423502" MODIFIED="1408028705135" TEXT="Peterson,2009,DEPTOR is an mTOR inhibitor frequently overexpressed in multiple myeloma cells and required for their survival"/>
 </node>
-<node CREATED="1408028728578" ID="ID_1268811785" MODIFIED="1408028739186" TEXT="Tti1/Tel2">
+<node CREATED="1408028728578" ID="ID_1268811785" MODIFIED="1408091367769" TEXT="Tti1/Tel2">
 <node CREATED="1408028739188" ID="ID_1597805626" MODIFIED="1408028764067" TEXT="Kaizuka,2010,Tti1 and Tel2 are critical factors in mammalian target of rapamycin complex assembly"/>
 </node>
 </node>
 <node CREATED="1408027192636" ID="ID_1783508684" MODIFIED="1408027206159" TEXT="TORC1">
-<node CREATED="1408028822621" ID="ID_1204682408" MODIFIED="1408028825415" TEXT="RAPTOR">
+<node CREATED="1408028822621" ID="ID_1204682408" MODIFIED="1408091366987" TEXT="RAPTOR">
 <node CREATED="1408028825416" ID="ID_1760699561" MODIFIED="1408028829552" TEXT="Hara, 2002, Raptor, a binding partner of target of rapamycin (TOR), mediates TOR action"/>
 <node CREATED="1408028869603" ID="ID_1022837902" MODIFIED="1408028879574" TEXT="Kim, 2002,mTOR interacts with raptor to form a nutrient-sensitive complex that signals to the cell growth machinery"/>
 </node>
-<node CREATED="1408028896011" ID="ID_1527666511" MODIFIED="1408028899251" TEXT="PRAS40">
+<node CREATED="1408028896011" ID="ID_1527666511" MODIFIED="1408091364627" TEXT="PRAS40">
 <node CREATED="1408028899252" ID="ID_487419802" MODIFIED="1408028935799" TEXT="Sancak, 2007, PRAS40 is an insulin-regulated inhibitor of the mTORC1 protein kinase"/>
 <node CREATED="1408028941953" ID="ID_1505107171" MODIFIED="1408029020274" TEXT="Thidieck, 2007, PRAS40 and PRR5-like protein are new mTOR interactors that regulate apoptosis"/>
 <node CREATED="1408029108153" ID="ID_1763276792" MODIFIED="1408029134826" TEXT="Vander e Haar, 2007, Insulin signalling to mTOR mediated by the Akt/PKB substrate PRAS40"/>
 <node CREATED="1408029215084" ID="ID_909850369" MODIFIED="1408029219367" TEXT="Wang, 2007, PRAS40 regulates mTORC1 kinase activity by functioning as a direct inhibitor of substrate binding"/>
 </node>
 </node>
-<node CREATED="1408027206868" ID="ID_1440881776" MODIFIED="1408027208559" TEXT="TORC2"/>
+<node CREATED="1408027206868" ID="ID_1440881776" MODIFIED="1408027208559" TEXT="TORC2">
+<node CREATED="1408029313894" ID="ID_1793966871" MODIFIED="1408091363517" TEXT="RICTOR">
+<node CREATED="1408029316271" ID="ID_1601813477" MODIFIED="1408029340615" TEXT="Jacinto, 2004, Mammalian TOR complex 2 controls the actin cytoskeleton and is rapamycin insensitive "/>
+<node CREATED="1408029390219" ID="ID_161745589" MODIFIED="1408029408276" TEXT="Sarbassov, 2004, Rictor, a novel binding partner of mTOR, defines a rapamycin-insensitive and raptor-independent pathway that regulates the cytoskeleton"/>
+</node>
+<node CREATED="1408029456222" ID="ID_213806273" MODIFIED="1408091361570" TEXT="mSin1">
+<node CREATED="1408029460817" ID="ID_347428957" MODIFIED="1408029474641" TEXT="Frias, 2006, mSin1 is necessary for Akt/PKB phosphorylation, and its isoforms define three distinct mTORC2s"/>
+<node CREATED="1408029519123" ID="ID_813015069" MODIFIED="1408029524588" TEXT="Jacinto, 2006, SIN1/MIP1 maintains rictor-mTOR complex integrity and regulates Akt phosphorylation and substrate specificity"/>
+</node>
+<node CREATED="1408029546010" ID="ID_1908491517" MODIFIED="1408091359841" TEXT="Protor1/2">
+<node CREATED="1408029551747" ID="ID_1960245816" MODIFIED="1408029584267" TEXT="Pearce, 2007, Identification of Protor as a novel Rictor-binding component of mTOR complex-2"/>
+<node CREATED="1408029604024" ID="ID_1024510012" MODIFIED="1408029691862" TEXT="Pearce, 2011, Protor-1 is required for efficient mTORC2-mediated activation of SGK1 in the kidney."/>
+<node CREATED="1408029712123" ID="ID_96331541" MODIFIED="1408029714309" TEXT="Thidieck, 2007, PRAS40 and PRR5-like protein are new mTOR interactors that regulate apoptosis "/>
+</node>
+</node>
+<node CREATED="1408091100537" ID="ID_1616161896" MODIFIED="1408091104212" TEXT="Inhibition">
+<node CREATED="1408091104889" ID="ID_1130245240" MODIFIED="1408091201615" TEXT="Brown, 1994, A mammalian protein targeted by G1-arresting rapamycin-receptor complex"/>
+<node CREATED="1408091386725" ID="ID_1870166517" MODIFIED="1408091388177" TEXT="Sabatini, 1994, RAFT1: a mammalian protein that binds to FKBP12 in a rapamycin-dependent fashion and is homologous to yeast TORs "/>
+</node>
 </node>
 </node>
 </map>
