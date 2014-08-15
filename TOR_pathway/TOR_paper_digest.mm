f48dd279a76fa326babde0c71cf5f6e9fc49e4dd
Sun Mai <sun.mai@outlook.com>
2014-08-15 14:32:37 +0200

    mindmap changes

--- TOR_pathway/TOR_paper_digest.mm
+++ TOR_pathway/TOR_paper_digest.mm
@@ -1,17 +1,19 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1408024907568" ID="ID_1553494727" MODIFIED="1408025340799" TEXT="TOR paper digest">
-<node CREATED="1408024957200" ID="ID_408854319" MODIFIED="1408025038410" POSITION="right" TEXT="Metabolism and disease">
-<node CREATED="1408025418340" ID="ID_440840791" MODIFIED="1408025457907" TEXT="Laplante &amp; Sabatini, 2012, mTOR Signaling in Growth  Control and Disease"/>
+<node CREATED="1408024957200" ID="ID_408854319" MODIFIED="1408094800324" POSITION="right" TEXT="Metabolism and disease">
+<node CREATED="1408025418340" ID="ID_440840791" MODIFIED="1408094833202" TEXT="Laplante &amp; Sabatini, 2012, mTOR Signaling in Growth  Control and Disease">
+<icon BUILTIN="idea"/>
 </node>
-<node CREATED="1408026118361" ID="ID_615067695" MODIFIED="1408026125778" POSITION="left" TEXT="History">
+</node>
+<node CREATED="1408026118361" FOLDED="true" ID="ID_615067695" MODIFIED="1408092517136" POSITION="left" TEXT="History">
 <node CREATED="1408026125716" ID="ID_809286609" MODIFIED="1408026176961" TEXT="Kunz, 1993, Target of rapamycin in yeast, TOR2, is an essential phosphatidylinositol kinase homolog required for G1 progression"/>
 <node CREATED="1408026199086" ID="ID_709148375" MODIFIED="1408026210927" TEXT="Cafferkey, 1993, Dominant missense mutations in a novel yeast protein related to mammalian phosphatidylinositol 3-kinase and VPS34 abrogate rapamycin cytotoxicity."/>
 <node CREATED="1408026345326" ID="ID_531446186" MODIFIED="1408026352281" TEXT="Brown, 1994, A mammalian protein targeted by G1-arresting rapamycin-receptor complex."/>
 <node CREATED="1408026353198" ID="ID_1244339472" MODIFIED="1408026872256" TEXT="Sabatini, 1994, RAFT1: a mammalian protein that binds to FKBP12 in a rapamycin-dependent fashion and is homologous to yeast TORs"/>
 <node CREATED="1408026907698" ID="ID_1279481596" MODIFIED="1408026919325" TEXT="Sabers, 1995, Isolation of a protein target of the FKBP12-rapamycin complex in mammalian cells"/>
 </node>
-<node CREATED="1408027148272" ID="ID_132060930" MODIFIED="1408091097099" POSITION="right" TEXT="Structure">
+<node CREATED="1408027148272" FOLDED="true" ID="ID_132060930" MODIFIED="1408092511449" POSITION="right" TEXT="Structure">
 <node CREATED="1408027151497" ID="ID_1094006752" MODIFIED="1408027197040" TEXT="shared">
 <node CREATED="1408027154592" ID="ID_1155809635" MODIFIED="1408091369355" TEXT="mLST1">
 <node CREATED="1408027161632" ID="ID_483991749" MODIFIED="1408027244269" TEXT="Jacinto, 2004, Mammalian TOR complex 2 controls the actin cytoskeleton and is rapamycin insensitive"/>
@@ -52,9 +54,48 @@
 <node CREATED="1408029712123" ID="ID_96331541" MODIFIED="1408029714309" TEXT="Thidieck, 2007, PRAS40 and PRR5-like protein are new mTOR interactors that regulate apoptosis "/>
 </node>
 </node>
-<node CREATED="1408091100537" ID="ID_1616161896" MODIFIED="1408091104212" TEXT="Inhibition">
+<node CREATED="1408091100537" ID="ID_1616161896" MODIFIED="1408091576164" TEXT="Inhibition">
 <node CREATED="1408091104889" ID="ID_1130245240" MODIFIED="1408091201615" TEXT="Brown, 1994, A mammalian protein targeted by G1-arresting rapamycin-receptor complex"/>
 <node CREATED="1408091386725" ID="ID_1870166517" MODIFIED="1408091388177" TEXT="Sabatini, 1994, RAFT1: a mammalian protein that binds to FKBP12 in a rapamycin-dependent fashion and is homologous to yeast TORs "/>
+<node CREATED="1408091576147" ID="ID_792062105" MODIFIED="1408091593117" TEXT="Integrity">
+<node CREATED="1408091562845" ID="ID_1000185265" MODIFIED="1408091564253" TEXT="Kim, 2002,mTOR interacts with raptor to form a nutrient-sensitive complex that signals to the cell growth machinery "/>
+</node>
+<node CREATED="1408091593851" ID="ID_1097525188" MODIFIED="1408091599918" TEXT="Kinase">
+<node CREATED="1408091600924" ID="ID_923231293" MODIFIED="1408091632148" TEXT="Yip, 2010, Structure of the human mTOR complex I and its implications for rapamycin inhibition"/>
+<node CREATED="1408092054917" ID="ID_642169349" MODIFIED="1408092060079" TEXT="Brunn, 1997, Phosphorylation of the translational repressor PHAS-I by the mammalian target of rapamycin."/>
+<node CREATED="1408092103283" ID="ID_1569718552" MODIFIED="1408092119430" TEXT="Burnett, 1998, RAFT1 phosphorylation of the translational regulators p70 S6 kinase and 4E-BP1"/>
+</node>
+</node>
+</node>
+<node CREATED="1408092520175" ID="ID_1107404280" MODIFIED="1408095169367" POSITION="right" TEXT="TORC1">
+<node CREATED="1408095143992" ID="ID_66885785" MODIFIED="1408095174357" TEXT="Upstream regulators">
+<node CREATED="1408092527320" ID="ID_1857395810" MODIFIED="1408092539209" TEXT="TSC1/2 &amp; Rheb">
+<node CREATED="1408092541982" ID="ID_1186044112" MODIFIED="1408092716991" TEXT="Inoki, 2003, Rheb GTPase is a direct target of TSC2 GAP activity and regulates mTOR signaling"/>
+<node CREATED="1408092816434" ID="ID_1446864446" MODIFIED="1408092821627" TEXT="Tee, 2003, Tuberous sclerosis complex gene products, Tuberin and Hamartin, control mTOR signaling by acting as a GTPase-activating protein complex toward Rheb"/>
+</node>
+<node CREATED="1408095202092" ID="ID_1075758959" MODIFIED="1408095207704" TEXT="Amino acids"/>
+<node CREATED="1408095208389" ID="ID_3936773" MODIFIED="1408095212660" TEXT="Stress"/>
+</node>
+<node CREATED="1408095178862" ID="ID_1255036745" MODIFIED="1408095500343" TEXT="Downstream events">
+<node CREATED="1408095500327" ID="ID_765094141" MODIFIED="1408095516209" TEXT="positive">
+<node CREATED="1408095220236" ID="ID_133060495" MODIFIED="1408095227775" TEXT="Lipids synthesis"/>
+<node CREATED="1408095213004" ID="ID_869010533" MODIFIED="1408095219567" TEXT="Protein synthesis"/>
+<node CREATED="1408095390020" ID="ID_1972300011" MODIFIED="1408095438381" TEXT="Energy consumption"/>
+</node>
+<node CREATED="1408095517798" ID="ID_1907646482" MODIFIED="1408095521862" TEXT="negative">
+<node CREATED="1408095521863" ID="ID_423003344" MODIFIED="1408095526600" TEXT="Autophagy"/>
+<node CREATED="1408095638615" ID="ID_61262069" MODIFIED="1408095647938" TEXT="Biosynthesis of lysosome"/>
+</node>
+</node>
+</node>
+<node CREATED="1408095897547" ID="ID_276423139" MODIFIED="1408095900251" POSITION="right" TEXT="TORC2">
+<node CREATED="1408095900253" ID="ID_1715202132" MODIFIED="1408095916851" TEXT="Upstream">
+<node CREATED="1408095916852" ID="ID_1233378693" MODIFIED="1408095920948" TEXT="PI3K"/>
+</node>
+<node CREATED="1408095906947" ID="ID_742768804" MODIFIED="1408095913038" TEXT="Downstream">
+<node CREATED="1408095925489" ID="ID_522156105" MODIFIED="1408095927196" TEXT="AKT"/>
+<node CREATED="1408095928002" ID="ID_978632467" MODIFIED="1408095933756" TEXT="SGK1"/>
+<node CREATED="1408095934744" ID="ID_1210695627" MODIFIED="1408096336617" TEXT="PKC-&#x3b1;"/>
 </node>
 </node>
 </node>
